e1325625db14b9514c1bb73aad4eadde90a4717e
waichun300 <wwchun0727@gmail.com>
2024-03-22 00:14:52 +0800

    Create login UI

--- app/src/main/java/my/edu/utar/fyp/LoginActivity.java
+++ app/src/main/java/my/edu/utar/fyp/LoginActivity.java
@@ -1,14 +1,97 @@
 package my.edu.utar.fyp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+    private TextView registerTextView;
+    private Button loginButton;
+    private EditText inputEmail,inputPassword;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerTextView = findViewById(R.id.register);
+        loginButton = findViewById(R.id.loginButton);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        progressDialog = new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performLogin();
+            }
+        });
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void performLogin() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        if(email.isEmpty())
+        {
+            inputEmail.setError("Enter Valid Email");
+        }
+        else if(password.isEmpty() || password.length()<6)
+        {
+            inputPassword.setError("Enter Valid Password");
+        }
+        else {
+            progressDialog.setMessage("Please wait till Login...");
+            progressDialog.setTitle("LoggedIn");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        nextActivity();
+                        Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                    }else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this,""+task.getException(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void nextActivity() {
+        Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/my/edu/utar/fyp/RegisterActivity.java
@@ -0,0 +1,132 @@
+package my.edu.utar.fyp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText registerEmail,registerPassword,registerAge;
+    private Button registerButton;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        registerEmail = findViewById(R.id.registerEmail);
+        registerPassword = findViewById(R.id.registerPassword);
+        registerButton = findViewById(R.id.registerButton);
+        registerAge = findViewById(R.id.registerAge);
+        progressDialog = new ProgressDialog(this);
+        mAuth= FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
+        Drawable drawable = getResources().getDrawable(R.drawable.age_icon);
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+        Bitmap bitmap = bitmapDrawable.getBitmap();
+
+        int desiredWidth = 100;
+        int desiredHeight = 100;
+
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, desiredWidth, desiredHeight, true);
+
+        Drawable resizedDrawable = new BitmapDrawable(getResources(), resizedBitmap);
+
+        registerAge.setCompoundDrawablesWithIntrinsicBounds(resizedDrawable, null, null, null);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registration();
+            }
+        });
+    }
+
+    private void registration() {
+        String email = registerEmail.getText().toString();
+        String password = registerPassword.getText().toString();
+        int age = Integer.parseInt(registerAge.getText().toString());
+        if(email.isEmpty())
+        {
+            registerEmail.setError("Enter Valid Email");
+        }
+        else if(password.isEmpty() || password.length()<6)
+        {
+            registerPassword.setError("Enter Valid Password");
+        }
+        else
+        {
+            progressDialog.setMessage("Please wait till Registration...");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        saveUserDetail(age);
+                    }
+                    else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this,""+task.getException(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void saveUserDetail(int age) {
+        String userId = mAuth.getCurrentUser().getUid();
+
+        DatabaseReference usersRef = FirebaseDatabase.getInstance("https://finalyearproject-ab7cc-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("Users");
+
+        usersRef.child("age").setValue(age)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()) {
+                            nextActivity();
+                            Toast.makeText(RegisterActivity.this,"Registration Successful",Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(RegisterActivity.this,"Failed to register",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void nextActivity() {
+        Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+        startActivity(intent);
+    }
+}
